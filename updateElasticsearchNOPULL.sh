c904f747964612666eb1207eb433ae05b3f9de8f
Elijah Gibson <82835625+semphorin@users.noreply.github.com>
2023-11-27 18:32:50 -0500

    Create updateElasticsearchNOPULL.sh

---
+++ updateElasticsearchNOPULL.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/SecurityOnion
+# remove old ES files
+sudo rm -rf /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
+sudo rm -rf /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
+sudo rm -f /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
+sudo rm -f /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
+echo Removed old files...
+# copy over new files
+cd /home/SecurityOnion/salt/sensoroni/files/analyzers
+sudo cp -r elasticsearch /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
+sudo cp -r elasticsearch /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
+cd /home/SecurityOnion/salt/sensoroni/
+sudo cp defaults.yaml /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
+sudo cp soc_sensoroni.yaml /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
+echo Copied new files.
+sudo so-sensoroni-restart --force
+echo Complete.
