7698adc1368a47ea5bb6f422ba572ac9539f9a56
Elijah Gibson <82835625+semphorin@users.noreply.github.com>
2023-11-27 18:15:17 -0500

    Add files via upload

---
+++ updateElasticsearch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/analyst/securityonion
+# update to latest
+sudo git pull origin 2.4/dev
+# remove old ES files
+sudo rm -rf /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
+sudo rm -rf /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
+sudo rm -f /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
+sudo rm -f /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
+# copy over new files
+cd /home/analyst/securityonion/salt/sensoroni/files/analyzers
+sudo cp -r elasticsearch /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
+sudo cp -r elasticsearch /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
+cd /home/analyst/securityonion/salt/sensoroni/
+sudo cp defaults.yaml /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
+sudo cp soc_sensoroni.yaml /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
+
