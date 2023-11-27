82d035a5daa1bce20a3ec73f9c39c3c0cb6b4281
Elijah Gibson <82835625+semphorin@users.noreply.github.com>
2023-11-27 18:28:33 -0500

    Update updateElasticsearch.sh

--- updateElasticsearch.sh
+++ updateElasticsearch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/analyst/securityonion
+cd /home/SecurityOnion
 # update to latest
 sudo git pull origin 2.4/dev
 echo Pulled latest edition.
@@ -10,10 +10,10 @@ sudo rm -f /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
 sudo rm -f /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
 echo Removed old files...
 # copy over new files
-cd /home/analyst/securityonion/salt/sensoroni/files/analyzers
+cd /home/SecurityOnion/salt/sensoroni/files/analyzers
 sudo cp -r elasticsearch /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
 sudo cp -r elasticsearch /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
-cd /home/analyst/securityonion/salt/sensoroni/
+cd /home/SecurityOnion/salt/sensoroni/
 sudo cp defaults.yaml /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
 sudo cp soc_sensoroni.yaml /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
 echo Copied new files.
